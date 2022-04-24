66e3fd1d0ae96716134a57917ab891e45fb3cccf
artem-brezutsky <a.brezutsky@gmail.com>
2022-04-24 23:25:05 +0300

    commit

--- main.go
+++ main.go
@@ -64,25 +64,18 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 func updateMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	var updatedMovie Movie
+
 	for index, item := range movies {
 		if item.ID == params["id"] {
-			movies[index] = Movie{
-				ID:    params["id"],
-				Isbm:  "12355",
-				Title: "New Title",
-				Director: &Director{
-					Firstname: "John_" + params["id"],
-					Lastname:  "Doe_" + params["id"],
-				},
-			}
-
-			updatedMovie = movies[index]
-			break
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
 		}
 	}
-	json.NewEncoder(w).Encode(updatedMovie)
-	return
 }
 
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
